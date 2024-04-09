f361fd48c70d0b2937d09dbceccd4d90c22b2882
sourav kumar <srvdhiman1998@gmail.com>
2024-04-09 15:01:43 +0530

    added new endpoint

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/app', function(req, res, next) {
+  res.send('respond with a resource app');
+});
 
 module.exports = router;
